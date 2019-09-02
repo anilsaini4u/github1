e8d627e6e96a1eaf1b08256b892f564e91baec74
anilsaini4u <anilsaini4u@gmail.com>
2019-09-02 07:00:02 +0000

    new change

--- firstjava.java
+++ firstjava.java
@@ -4,8 +4,8 @@ class firstjava
 public static void main(String [] args)
 {
 firstjava j1 = new firstjava();
-	System.out.println("Hello World");
-	System.out.println("Hello World2");
+	System.out.println("Hello World 1");
+	System.out.println("Hello World 2");
 }
 
 }
